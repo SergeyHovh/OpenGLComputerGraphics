420c27cc901d86c30083f472c38db7bd720d61cf
SergeyHovh <sergohovhyan@gmail.com>
2019-04-05 15:22:53 +0400

    drawn a triangle w/o a shader

--- Sphere/main.cpp
+++ Sphere/main.cpp
@@ -26,13 +26,29 @@ int main(void)
 	if (glewInit() != GLEW_OK)
 		std::cout << "fok" << std::endl;
 
-	std::cout << glGetString(GL_VERSION) << std::endl;
+	float positions[]
+	{
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
 
+	// bind the buffer
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	// setup layout
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
